63a20dbf7687bc1e964f627840f1b8b9a18d1d31
Thomas Jessup Dix <tjd44264@uga.edu>
2026-01-18 17:05:12 -0500

    Implemented default thumbnail
    Books now have a default thumbnail set for if API lacks a thumbnail for the given book.

--- library-app/src/app/components/Book.tsx
+++ library-app/src/app/components/Book.tsx
@@ -18,20 +18,27 @@ type Book = {
 
 export default function Book() {
     const [books, setBooks] = useState<Book[]>([]);
+    
 
     useEffect(() => {
-        fetch("http://localhost:8080/api/books/search?query=he-who-fights-with-monsters")
+        fetch("http://localhost:8080/api/books/search?query=the+lord+of+the+rings")
             .then(res => res.json())
             .then((data: Book[]) => setBooks(data))
             .catch(err => console.error("Failed to fetch books", err));
+
+
     }, []);
+
+    const urlDisp = (props: Book) => {
+        return props.thumbnail ?? "https://storage.needpix.com/rsynced_images/red-146613_1280.png";
+    }
 
     return (
         <div className="bookcon">
             {books.map(book => (
                 <Link href={`/books/${book.id}`} prefetch className="bookcard" key={book.id}>
                     <div className='image-container'>
-                        <img src={book.thumbnail} alt={`${book.title} cover`} />
+                        <img src={urlDisp(book)} alt={`${book.title} cover`} />
                     </div>
                     <h2><strong>{book.title}</strong></h2>
                     <p>{book.author}</p>
